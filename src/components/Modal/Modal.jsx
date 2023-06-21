1d100b66dbd3c97ec51113e6071dcd9e48b21179
AndriiRod <rod.andriiw@gmail.com>
2023-06-21 17:39:37 +0300

    add

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -8,10 +8,12 @@ const modalRoot = document.getElementById('modal-root');
 class Modal extends Component {
   componentDidMount() {
     window.addEventListener('keydown', this.handleKeyDown);
+    document.body.style.overflow = 'hidden';
   }
 
   componentWillUnmount() {
     window.removeEventListener('keydown', this.handleKeyDown);
+    document.body.style.overflow = 'visible';
   }
 
   handleKeyDown = e => {
